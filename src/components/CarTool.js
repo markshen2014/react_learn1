59985ec58839a917ecc7ed8405b0543fc2a0db84
adm-mshen <mshen@synergynetworx>
2019-08-22 15:37:31 -0400

    test1

---
+++ src/components/CarTool.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export const CarTool = (props) => {
+    
+    console.log(Object.isFrozen(props));
+
+    return (
+     <>   
+    <header>
+        <h1>Car Tools</h1>
+    </header>
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Make</th>
+                <th>Model</th>
+                <th>Year</th>
+                <th>Color</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+        {props.cars.map( car =>
+             <tr key={car.id}>
+               <td >{car.id}</td>
+               <td>{car.make}</td>
+               <td>{car.model}</td>
+               <td>{car.year}</td>
+               <td>{car.color}</td>
+               <td>{car.price}</td>
+             </tr>
+            )}
+        </tbody>
+    </table>
+    </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { CarTool } from './components/CarTool'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const carList = [
+    {id: 1, make: 'Ford', model: 'Fusion Hybrid', year: 2018, color: 'silver', price:30000},
+    {id: 2, make: 'Tesla', model: 'S', year: 2017, color: 'red', price:120000}
+ ];
+
+ReactDOM.render(<CarTool cars={carList}/>,document.getElementById('root'));
